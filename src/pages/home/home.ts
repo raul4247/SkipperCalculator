e0a98fb0919d365c5ec08f9105e7a3bc220fac07
Raul Mansur <raulfmansur@hotmail.com>
2018-06-14 00:53:04 -0300

    v0.1

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,13 +2,21 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+	selector: 'page-home',
+	templateUrl: 'home.html'
 })
 export class HomePage {
+	numClasses: number = 34;
+	skippedClasses: number = 7;
+	minPercentage: number = 25;
 
-  constructor(public navCtrl: NavController) {
+	myPercent: number;
+	myPossibleSkips: number;
 
-  }
+	constructor(public navCtrl: NavController){}
 
+	calculate(){
+		this.myPercent = Number((this.skippedClasses/this.numClasses * 100).toFixed(2));
+		this.myPossibleSkips = Math.floor((this.numClasses * this.minPercentage * 0.01) - this.skippedClasses);
+	}
 }
